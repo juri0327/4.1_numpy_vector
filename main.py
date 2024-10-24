65ef6ec81c4f4e7a85125dab718c3586fc2b3869
juri0327 <8721089@ed.tus.ac.jp>
2024-10-24 16:55:07 +0900

    課題完了

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 import numpy as np
 
 def list2ndarray(input_list):
-    pass
+    return np.array(input_list)
 
 def dist(x_array, y_array):
-    pass
+    return np.linalg.norm(x_array - y_array)
 
 def extract(x_array):
-    pass
+    return x_array[1:-1]
 
 def drop(x_array, idx):
-    pass
+    x_array[idx] = 0
+    return x_array
